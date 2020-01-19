e509078e5a71e400543c13af15b68661aa70c010
Jeong Hun <wanted0301@gmail.com>
2020-01-19 22:20:04 +0900

    200119 update
    java variable tutorial update

---
+++ 3.VariableTutorial/src/VariableTutorial.java
@@ -0,0 +1,12 @@
+public class VariableTutorial {
+
+    public static void main(String[] args){
+        int a; // 변수 선언 - 변수의 이름
+        a = 1; // a는 1이다.
+        System.out.println(a+1); // a는 1이고 그에 1을 더하면 2가 출력된다.
+
+        a = 2; // a는 2이다.
+        System.out.println(a+1); // a는 2이고 그에 2를 더하면 3이 출력된다.
+    }
+
+}
